3e00d543b7ce7d4e2744d9c0b483ae100a67168c
Oden <oden.srtk@gmail.com>
2022-11-16 21:39:27 +0900

    add conf file setting

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::collections::HashMap;
-use std::io::{Error, Write, BufReader, BufRead};
+use std::io::{Error, Write, BufReader, BufRead, Read};
 use std::net::{TcpListener, TcpStream};
 use std::sync::{Arc, Mutex};
 use std::thread;
+
+#[macro_use]
+extern crate serde_derive;
 
 use wild_doc::{
     WildDoc
@@ -49,41 +52,62 @@ impl<'a> IncludeAdaptor for IncludeRemote{
     }
 }
 
+#[derive(Deserialize)]
+struct Config{
+    wilddoc: Option<ConfigWildDoc>
+}
+#[derive(Deserialize)]
+struct ConfigWildDoc {
+    path: Option<String>
+}
+
 fn main() {
-    let dir="./wd-test/";
-    
-    let mut wild_docs=HashMap::new();
-    let listener=TcpListener::bind("localhost:51818").expect("Error. failed to bind.");
-    for streams in listener.incoming(){
-        match streams {
-            Err(e) => { eprintln!("error: {}", e)},
-            Ok(stream)=>{
-                let mut buffer=Vec::new();
-                
-                let mut tcp_reader=BufReader::new(&stream);
-                tcp_reader.read_until(0,&mut buffer).unwrap();
-
-                buffer.remove(buffer.len()-1);
-                if let Ok(dbname)=std::str::from_utf8(&buffer){
-                    let dir=dir.to_owned()+dbname+"/";
-                    if std::path::Path::new(&dir).exists(){
-                        std::fs::remove_dir_all(&dir).unwrap();
-                        std::fs::create_dir_all(&dir).unwrap();
-                    }else{
-                        std::fs::create_dir_all(&dir).unwrap();
+    if let Ok(mut f)=std::fs::File::open("wild-doc.toml"){
+        let mut toml=String::new();
+        if let Ok(_)=f.read_to_string(&mut toml){
+            let config: Result<Config, toml::de::Error> = toml::from_str(&toml);
+            if let Ok(config)=config{
+                if let Some(config)=config.wilddoc{
+                    if let Some(dir)=config.path{
+                        let mut wild_docs=HashMap::new();
+                        let listener=TcpListener::bind("localhost:51818").expect("Error. failed to bind.");
+                        for streams in listener.incoming(){
+                            match streams {
+                                Err(e) => { eprintln!("error: {}", e)},
+                                Ok(stream)=>{
+                                    let mut buffer=Vec::new();
+                                    
+                                    let nbytes={
+                                        let mut tcp_reader=BufReader::new(&stream);
+                                        tcp_reader.read_until(0,&mut buffer).unwrap()
+                                    };
+                                    if nbytes>0{
+                                        buffer.remove(buffer.len()-1);
+                                        if let Ok(dbname)=std::str::from_utf8(&buffer){
+                                            let dir=dir.to_owned()+dbname+"/";
+                                            let wd=wild_docs.entry(dir).or_insert_with_key(|dir|{
+                                                if !std::path::Path::new(dir).exists(){
+                                                    std::fs::create_dir_all(dir).unwrap();
+                                                }
+                                                Arc::new(Mutex::new(WildDoc::new(dir,IncludeEmpty::new()).unwrap()))
+                                            });
+                                            let wd=Arc::clone(&wd);
+                                            thread::spawn(move || {
+                                                handler(stream,wd).unwrap_or_else(|error| eprintln!("handler {:?}", error));
+                                            });
+                                        }
+                                    }else{
+                                        println!("recv 0 bytes");
+                                    }
+                                }
+                            }
+                        }
                     }
-
-                    let wd=wild_docs.entry(dir).or_insert_with_key(|key|{
-                        Arc::new(Mutex::new(WildDoc::new(key,IncludeEmpty::new()).unwrap()))
-                    });
-                    let wd=Arc::clone(&wd);
-                    thread::spawn(move || {
-                        handler(stream,wd).unwrap_or_else(|error| eprintln!("handler {:?}", error));
-                    });
                 }
             }
         }
     }
+    
 }
 
 fn handler<T:IncludeAdaptor>(
