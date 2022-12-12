d0f1c835d99a7adad1052ce5b6a2c87ee2ae1039
Oden <oden.srtk@gmail.com>
2022-12-12 22:11:40 +0900

    update wild-doc

--- src/include.rs
+++ src/include.rs
@@ -1,39 +1,40 @@
-use std::{net::TcpStream, io::{Write, BufReader, BufRead}};
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::TcpStream,
+};
 
 use wild_doc::IncludeAdaptor;
 
-pub struct IncludeEmpty{}
-impl IncludeEmpty{
-    pub fn new()->Self{
-        Self{}
+pub struct IncludeEmpty {}
+impl IncludeEmpty {
+    pub fn new() -> Self {
+        Self {}
     }
 }
-impl IncludeAdaptor for IncludeEmpty{
-    fn include(&mut self,_:&str)->String{
+impl IncludeAdaptor for IncludeEmpty {
+    fn include(&mut self, _: &str) -> String {
         "".to_string()
     }
 }
 
-pub struct IncludeRemote{
-    stream:TcpStream
+pub struct IncludeRemote {
+    stream: TcpStream,
 }
-impl IncludeRemote{
-    pub fn new(stream:TcpStream)->Self{
-        Self{
-            stream
-        }
+impl IncludeRemote {
+    pub fn new(stream: TcpStream) -> Self {
+        Self { stream }
     }
 }
-impl<'a> IncludeAdaptor for IncludeRemote{
-    fn include(&mut self,path:&str)->String {
-        let _=self.stream.write(("include:".to_owned()+path).as_bytes());
-        let _=self.stream.write(&[0]);
+impl<'a> IncludeAdaptor for IncludeRemote {
+    fn include(&mut self, path: &str) -> String {
+        let _ = self.stream.write(("include:".to_owned() + path).as_bytes());
+        let _ = self.stream.write(&[0]);
         let mut reader = BufReader::new(&self.stream);
         let mut recv_response = Vec::new();
-        if let Ok(v)=reader.read_until(0,&mut recv_response) {
+        if let Ok(v) = reader.read_until(0, &mut recv_response) {
             if v > 0 {
-                recv_response.remove(recv_response.len()-1);
-                if let Ok(xml)=std::string::String::from_utf8(recv_response){
+                recv_response.remove(recv_response.len() - 1);
+                if let Ok(xml) = std::string::String::from_utf8(recv_response) {
                     return xml;
                 }
             }
